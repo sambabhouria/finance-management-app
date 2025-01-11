e418f1882eacada7af2276a7f9f19bd2d8a57267
DIALLO <sambabhouria@gmail.com>
2025-01-11 17:18:32 +0100

    add proifle and transactions boilerplate screen

---
+++ app/(tabs)/transactions.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+type Props = {}
+
+const Transactions = (props: Props) => {
+  return (
+    <View>
+      <Text>Transactions</Text>
+    </View>
+  )
+}
+
+export default Transactions
+
+const styles = StyleSheet.create({})
